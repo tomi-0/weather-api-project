5bc3905c67face2380d3dce78f143bf6f132b4ee
tomi-0 <asadetomi@gmail.com>
2026-01-30 01:19:03 +0000

    script files set up

---
+++ scripts/app.js
@@ -0,0 +1,24 @@
+
+const city = "London"
+// API_KEY was declared in a global script tag, it’s now available for you to use in your fetch request:
+
+// 1. The "Brain" (Global Variables & Selectors)
+// store all dunamic HTML elemetns
+const searchBar = document.querySelector('search-input')
+const submitButton = document.querySelector('search-icon')
+const weatherIcon = document.querySelector('weather-emoji')
+
+// 2. The "Messenger" (The API Logic)
+
+
+fetchData()
+// 3. The "Translator" (Data Processing)
+
+
+
+
+// 4. The "Decorator" (DOM Manipulation)
+
+
+
+// 5. The "Triggers" (Event Listeners)
